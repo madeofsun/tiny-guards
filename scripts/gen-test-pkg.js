5b483bfe106353854df8cdc06911ce19c1d67055
madeofsun <rettou74@gmail.com>
2023-08-01 22:46:59 +0300

    fix test-pkg

--- scripts/gen-test-pkg.js
+++ scripts/gen-test-pkg.js
@@ -9,5 +9,7 @@ async function main() {
   process.chdir(new URL("..", import.meta.url).pathname);
 
   const modules = await collectModules();
-  await generateTestPkgIndex(modules);
+  await generateTestPkgIndex(
+    modules.filter((module) => module !== "devLogger")
+  );
 }
