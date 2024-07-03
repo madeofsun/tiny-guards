5c3fc32de43f956574dbc4a7bb8fc791f581ef07
madeofsun <rettou74@gmail.com>
2024-07-03 19:34:32 +0300

    v3

--- test-pkg/esm/extra.js
+++
@@ -1,8 +0,0 @@
-import { Guards } from "tiny-guards";
-if (typeof Guards === "undefined") {
-  throw new Error("Guards is not defined");
-}
-import { Guards as Guards$2 } from "tiny-guards/guards";
-if (typeof Guards$2 === "undefined") {
-  throw new Error("Guards$2 is not defined");
-}
--- test-pkg/ts/extra-types.ts
+++ test-pkg/ts/extra-types.ts
@@ -1,23 +1,30 @@
 import {
-  Primitive,
-  Shape,
   Guard,
   GuardInfer,
+  ComplexGuard,
+  GuardError,
+  GuardLog,
+  GuardLogItem,
   Refinement,
   Narrowing,
+  Primitive,
   AnyConstructor,
   AnyFunction,
+  Shape,
 } from "tiny-guards";
 
 import {
   Guard as Guard2,
   GuardInfer as GuardInfer2,
+  ComplexGuard as ComplexGuard2,
+  GuardError as GuardError2,
+  GuardLog as GuardLog2,
+  GuardLogItem as GuardLogItem2,
   Refinement as Refinement2,
   Narrowing as Narrowing2,
 } from "tiny-guards/types";
 
 import { Primitive as Primitive2 } from "tiny-guards/isPrimitive";
-
 import { AnyConstructor as AnyConstructor2 } from "tiny-guards/instance";
 import { AnyFunction as AnyFunction2 } from "tiny-guards/isFunction";
 import { Shape as Shape2 } from "tiny-guards/shape";
