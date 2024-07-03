745423861d80141802fe0842911bb99971cc8b6d
madeofsun <rettou74@gmail.com>
2024-07-03 18:13:33 +0300

    v3

--- tests/error.test.ts
+++ tests/error.test.ts
@@ -1,22 +1,22 @@
 import { array } from "../src/array";
-import { Guards } from "../src/guards";
 
 describe("Guards", () => {
   describe("error", () => {
     test("throw on first", () => {
-      expect(() => Guards.error).toThrow("Invalid usage");
+      const isArray = array();
+      expect(() => isArray.error).toThrow("Invalid usage");
     });
 
     test("throw on valid", () => {
       const isArray = array();
       isArray([]);
-      expect(() => Guards.error).toThrow("Invalid usage");
+      expect(() => isArray.error).toThrow("Invalid usage");
     });
 
     test("do not throw on invalid", () => {
       const isArray = array();
       isArray(1);
-      expect(Guards.error.message).toMatch(/^validation failed\n\[isArray\]/);
+      expect(isArray.error.message).toMatch(/^validation failed\n\[isArray\]/);
     });
   });
 });
