fb9bc18674253e8a8ceba83957d1c472c2e7f67f
madeofsun <rettou74@gmail.com>
2024-10-13 16:26:59 +0300

    v3

--- tests/predicate.test.ts
+++ tests/predicate.test.ts
@@ -1,13 +1,9 @@
-import { p, p } from "../src/p";
+import { p } from "../src/p";
 
 describe(p, () => {
   test(p.name, () => {
     const some = p("qwe", () => true);
 
     expect(some.name).toBe("qwe");
-  });
-
-  test("p", () => {
-    expect(p).toBe(p);
   });
 });
